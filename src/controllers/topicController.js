236c603eb01e111f0cc35788695839b7fcdfe535
Matt Outen <mattoutenphysed@gmail.com>
2018-09-02 19:53:07 -0400

    commit after delete attempt, error posting delete

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -9,5 +9,40 @@ module.exports = {
         res.render("topics/index", {title: "Welcome to Bloccit", topics});
       }
     });
-  }
+  },
+  new(req, res, next){
+    res.render("topics/new");
+  },
+  create(req, res, next){
+    let newTopic = {
+      title: req.body.title,
+      description: req.body.description
+    };
+    topicQueries.addTopic(newTopic, (err, topic) => {
+      if(err){
+        res.redirect(500, '/topics/new');
+      } else {
+        res.redirect(303, `/topics/${topic.id}`)
+      }
+    });
+  },
+  show(req, res, next){
+    topicQueries.getTopic(req.params.id, (err, topic) => {
+      if(err || topic == null){
+        res.redirect(404, "/");
+        } else {
+          res.render("topics/show", {topic});
+        }
+    });
+  },
+  destroy(req, res, next){
+    topicQueries.deleteTopic(req.params.id, (err, topic) => {
+      if(err) {
+        res.redirect(500, `/topics/${topic.id}`)
+      } else {
+          res.redirect(303, "/topics")
+        }
+    });
+  },
+      
 }
--- src/routes/topics.js
+++ src/routes/topics.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
 const topicController = require("../controllers/topicController")
 
 router.get("/topics", topicController.index);
+router.get('/topics/new', topicController.new);
+router.post("/topics/create", topicController.create);
+router.get("/topics/:id", topicController.show);
+router.post("topics/:id/destroy", topicController.destroy);
+
 
 module.exports = router;
