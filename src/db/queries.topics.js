5de245fb488a702e09d3be824242abc53e899546
Matt Outen <mattoutenphysed@gmail.com>
2018-09-09 13:26:30 -0500

    checkpoint-8 complete

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,5 +1,7 @@
 const Topic = require('./models').Topic;
 
+const Post = require('./models').Post;
+
 module.exports = {
     getAllTopics(callback){
         return Topic.all()
@@ -12,7 +14,12 @@ module.exports = {
     },
 
     getTopic(id, callback){
-        return Topic.findById(id)
+      return Topic.findById(id, {
+        include: [{
+          model: Post,
+          as: 'posts'
+        }]
+      })
         .then((topic) => {
           callback(null, topic);
         })
