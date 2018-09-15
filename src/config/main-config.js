6c2e0152657e55387e4bf06cd5710cb3e1175531
Matt Outen <mattoutenphysed@gmail.com>
2018-09-14 21:08:00 -0500

    error on update validate checkpoint

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -11,7 +11,6 @@ module.exports = {
       app.set('views', viewsFolder);
       app.set('view engine', 'ejs');
       app.use(bodyParser.urlencoded({ extended: true }));
-      app.use(express.static(path.join(__dirname, '..', 'assets')));
       app.use(expressValidator());
       app.use(session({
         secret: process.env.cookieSecret,
@@ -20,5 +19,6 @@ module.exports = {
         cookie: { maxAge: 60000 }
       }));
       app.use(flash());
+      app.use(express.static(path.join(__dirname, '..', 'assets')));
   }
 };
