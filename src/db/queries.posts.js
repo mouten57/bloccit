b2555894827ef97972760f22f3018f2277475533
Matt Outen <mattoutenphysed@gmail.com>
2018-09-20 21:09:40 -0500

    voting checkpoint done

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -3,6 +3,7 @@ const Topic = require('./models').Topic;
 const Comment = require('./models').Comment;
 const User = require("./models").User;
 const Authorizer = require('../policies/application');
+const Vote = require("./models").Vote;
 
 module.exports = {
     addPost(newPost, callback) {
@@ -19,7 +20,7 @@ module.exports = {
             include: [
                 {model: Comment, as: "comments", include: [
                     {model: User }
-                ]}
+                ]}, {model: Vote, as: "votes"}
             ]
         })
         .then((post) => {
