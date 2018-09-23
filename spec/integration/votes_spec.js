6676f013c03976f281fe110a08fba47a0a158497
Matt Outen <mattoutenphysed@gmail.com>
2018-09-23 08:33:34 -0500

    profiles checkpoint: COMPLETE

--- spec/integration/votes_spec.js
+++ spec/integration/votes_spec.js
@@ -78,7 +78,7 @@ describe("routes : votes", () => {
               (err, res, body) => {
                 Vote.findOne({            // look for the vote, should not find one.
                   where: {
-                    userId: this.user.id,
+                    userId: 0,
                     postId: this.post.id
                   }
                 })
